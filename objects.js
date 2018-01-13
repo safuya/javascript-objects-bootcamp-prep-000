08c246d016034b1ad7282adadc934019907620d1
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-13 05:32:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,8 @@ var playlist = {Angra: 'Travellers of Time'}
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, {[artistName]: songTitle})
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
