7fba01d1abbfcecd6f444fc7e1064394270dd7b0
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-13 05:18:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,6 @@
 let playlist = {Angra: 'Travellers of Time'}
 
-console.log(playlist)
-
 function updatePlaylist(obj, artistName, songTitle) {
   obj[artistName] = songTitle
   return obj
 }
-
-playlist = updatePlaylist({}, 'Palov', "What's that")
-console.log(playlist)
